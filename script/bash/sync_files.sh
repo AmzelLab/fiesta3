5839a6fc45a8cddd8d988c15d52cb8df18bbf209
Yunlong Liu <davislong198833@gmail.com>
2016-11-04 14:47:24 -0400

    fix bugs

--- script/bash/sync_files.sh
+++ script/bash/sync_files.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 #
 # Perform automatic sync service over local and remote
+# UNCOMMENT THIS WHILE DEBUGGING: set -x
 
 declare -a REMOTE_LOCATIONS_ARRAY=()
 declare -a LOCAL_LOCATIONS_ARRAY=()
 
 # Set Global Parameters HERE
-REMOTE_PREFIX=marcc:/home-4/yliu120@jhu.edu/scratch/
-LOCAL_PREFIX=/nfs/fs/amzel3/yliu120/
+REMOTE_PREFIX=marcc:/home-4/yliu120@jhu.edu/scratch
+LOCAL_PREFIX=/nfs/fs/amzel3/yliu120
 SYNC_JOB_NUM=1
 SLEEP_TIME=172800
 
 register_entry() {
-  REMOTE_LOCATIONS_ARRAY+=$1
-  LOCAL_LOCATIONS_ARRAY+=$2
-  SYNC_JOB_NUM=SYNC_JOB_NUM+1
+  REMOTE_LOCATIONS_ARRAY[${SYNC_JOB_NUM}]=$1
+  LOCAL_LOCATIONS_ARRAY[${SYNC_JOB_NUM}]=$2
+  ((SYNC_JOB_NUM++))
 }
 
 run_sync() {
   for ((i=1;i<SYNC_JOB_NUM;i++)); do
-    rsync -rvlt ${REMOTE_PREFIX}${REMOTE_LOCATIONS_ARRAY[$i]} \
-      ${LOCAL_PREFIX}${LOCAL_LOCATIONS_ARRAY[$i]}/ &
+    (rsync -rvlt ${REMOTE_PREFIX}/${REMOTE_LOCATIONS_ARRAY[$i]} \
+      ${LOCAL_PREFIX}/${LOCAL_LOCATIONS_ARRAY[$i]}/ &)
   done
 }
 
@@ -30,5 +31,5 @@ register_entry "nis_sandwich/production" "nis_sandwich"
 
 for ((;;)) do
   run_sync
-  sleep ${SLEEP_TIME} # two days.
+  sleep ${SLEEP_TIME} # sleep two days until next sync.
 done
