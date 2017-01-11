698aad23331b7f7f2b641151906b109a42ef90b4
Yunlong Liu <davislong198833@gmail.com>
2017-01-11 14:56:08 -0500

    complete simple_autoencoder.py

--- experimental/yliu120/tensorflow/simple_autoencoder.py
+++ experimental/yliu120/tensorflow/simple_autoencoder.py
@@ -7,19 +7,35 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import argparse
 import numpy as np
 import tensorflow as tf
 
 __author__ = 'Yunlong Liu (davislong198833@gmail.com)'
 
+DESCRIPTION = "Experiment 2-layer Autoencoder with tensorflow"
+
 INPUT_DIM = 1024
 HIDDEN_1_DIM = 256
 HIDDEN_2_DIM = 32
-BATCH_SIZE = 256
+
+NUM_BATCH = 20
+NUM_EPOCH = 50
 
 WEIGHTS = {
-    'encoder_h1': tf.Variable(tf.random_normal())
+    'encoder_h1': tf.Variable(tf.random_normal(INPUT_DIM, HIDDEN_1_DIM)),
+    'encoder_h2': tf.Variable(tf.random_normal(HIDDEN_1_DIM, HIDDEN_2_DIM)),
+    'decoder_h1': tf.Variable(tf.random_normal(HIDDEN_2_DIM, HIDDEN_1_DIM)),
+    'decoder_h2': tf.Variable(tf.random_normal(HIDDEN_1_DIM, INPUT_DIM)),
 }
+
+BIASE = {
+    'encoder_b1': tf.Variable(tf.random_normal([HIDDEN_1_DIM])),
+    'encoder_b2': tf.Variable(tf.random_normal([HIDDEN_2_DIM])),
+    'decoder_b1': tf.Variable(tf.random_normal([HIDDEN_1_DIM])),
+    'decoder_b2': tf.Variable(tf.random_normal([INPUT_DIM])),
+}
+
 
 def encoder(dummy_x):
     """Build the encoder graph
@@ -30,7 +46,12 @@ def encoder(dummy_x):
     Returns:
         The output dummy layer.
     """
-    pass
+    hidden_1 = tf.nn.sigmoid(tf.add(tf.matmul(dummy_x, WEIGHTS['encoder_h1']),
+                                    BIASE['encoder_b1']))
+    hidden_2 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_1, WEIGHTS['encoder_h2']),
+                                    BIASE['encoder_b2']))
+    return hidden_2
+
 
 def decoder(dummy_z):
     """Build the decoder graph
@@ -41,7 +62,25 @@ def decoder(dummy_z):
     Returns:
         The reconstruction of X, original data.
     """
-    pass
+    hidden_1 = tf.nn.sigmoid(tf.add(tf.matmul(dummy_z, WEIGHTS['decoder_h1']),
+                                    BIASE['decoder_b1']))
+    hidden_2 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_1, WEIGHTS['decoder_h2']),
+                                    BIASE['decoder_b2']))
+    return hidden_2
+
+
+def model(dummy_x):
+    """Build the entire autoencoder graph"""
+    encoder_graph = encoder(dummy_x)
+    decoder_graph = decoder(encoder_graph)
+
+    reconstruction = decoder_graph
+    original = dummy_x
+
+    loss_function = tf.reduce_mean(tf.pow(original - reconstruction, 2))
+    optimizer = tf.train.AdamOptimizer().minimize(loss_function)
+
+    return (loss_function, optimizer)
 
 
 def train(input_data):
@@ -51,12 +90,42 @@ def train(input_data):
     Args:
         input_data: np array. 2-dim.
     """
-    input_x = tf.placeholder(tf.float32, shape=(BATCH_SIZE, input_dim))
+    input_x = tf.placeholder(tf.float32, shape=(None, INPUT_DIM))
+
+    # Put input_x in our model
+    loss, optimizer = model(input_x)
+
+    # Initialize tf Session
+    init = tf.initialize_all_variables()
+
+    # Launch training process
+    with tf.Session() as session:
+        session.run(init)
+
+        # Training cycle
+        for epoch in range(NUM_EPOCH):
+            for batch in range(NUM_BATCH):
+                batch_x = input_data[batch::NUM_BATCH]
+                session.run(optimizer, feed_dict={input_x: batch_x})
+
+            # output logs per epoch
+            loss_per_epoch = session.run(loss, feed_dict={
+                input_x: input_data[:NUM_BATCH * NUM_EPOCH:1]})
+            print("Epoch: ", '%02d' % (epoch + 1),
+                  " cost: ", "{:.6f}".format(loss_per_epoch))
 
 
 def main():
     """The whole work flow of training the encoder"""
-    pass
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    # Positional Args
+    parser.add_argument('data', metavar='DATA', nargs='?',
+                        help='compressed Numpy data')
+    args = parser.parse_args()
+
+    train(np.load(args.data))
+
 
 if __name__ == "__main__":
-   main()
+    main()
